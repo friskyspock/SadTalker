abc44f72bd9aee46590fec42cd8f27290d7253ff
aniket <aniketms42@gmail.com>
2024-03-27 12:16:06 +0530

    added color matching algo

---
+++ src/utils/hist_matching.py
@@ -0,0 +1,74 @@
+import cv2 # Import the OpenCV library
+import numpy as np # Import Numpy library
+from numpy.typing import NDArray
+
+def calculate_cdf(histogram: NDArray) -> NDArray:
+    # Get the cumulative sum of the elements
+    cdf = histogram.cumsum()
+ 
+    # Normalize the cdf
+    normalized_cdf = cdf / float(cdf.max())
+ 
+    return normalized_cdf
+ 
+def calculate_lookup(src_cdf: NDArray, ref_cdf: NDArray) -> NDArray:
+    lookup_table = np.zeros(256)
+    lookup_val = 0
+    for src_pixel_val in range(len(src_cdf)):
+        lookup_val
+        for ref_pixel_val in range(len(ref_cdf)):
+            if ref_cdf[ref_pixel_val] >= src_cdf[src_pixel_val]:
+                lookup_val = ref_pixel_val
+                break
+        lookup_table[src_pixel_val] = lookup_val
+    return lookup_table
+ 
+def match_histograms(src_image, ref_image):
+    """
+    This method matches the source image histogram to the
+    reference signal
+    :param image src_image: The original source image
+    :param image  ref_image: The reference image
+    :return: image_after_matching
+    :rtype: image (array)
+    """
+    # Split the images into the different color channels
+    # b means blue, g means green and r means red
+    src_b, src_g, src_r = cv2.split(src_image)
+    ref_b, ref_g, ref_r = cv2.split(ref_image)
+ 
+    # Compute the b, g, and r histograms separately
+    # The flatten() Numpy method returns a copy of the array c
+    # collapsed into one dimension.
+    src_hist_blue, bin_0 = np.histogram(src_b.flatten(), 256, [0,256])
+    src_hist_green, bin_1 = np.histogram(src_g.flatten(), 256, [0,256])
+    src_hist_red, bin_2 = np.histogram(src_r.flatten(), 256, [0,256])    
+    ref_hist_blue, bin_3 = np.histogram(ref_b.flatten(), 256, [0,256])    
+    ref_hist_green, bin_4 = np.histogram(ref_g.flatten(), 256, [0,256])
+    ref_hist_red, bin_5 = np.histogram(ref_r.flatten(), 256, [0,256])
+ 
+    # Compute the normalized cdf for the source and reference image
+    src_cdf_blue = calculate_cdf(src_hist_blue)
+    src_cdf_green = calculate_cdf(src_hist_green)
+    src_cdf_red = calculate_cdf(src_hist_red)
+    ref_cdf_blue = calculate_cdf(ref_hist_blue)
+    ref_cdf_green = calculate_cdf(ref_hist_green)
+    ref_cdf_red = calculate_cdf(ref_hist_red)
+ 
+    # Make a separate lookup table for each color
+    blue_lookup_table = calculate_lookup(src_cdf_blue, ref_cdf_blue)
+    green_lookup_table = calculate_lookup(src_cdf_green, ref_cdf_green)
+    red_lookup_table = calculate_lookup(src_cdf_red, ref_cdf_red)
+ 
+    # Use the lookup function to transform the colors of the original
+    # source image
+    blue_after_transform = cv2.LUT(src_b, blue_lookup_table)
+    green_after_transform = cv2.LUT(src_g, green_lookup_table)
+    red_after_transform = cv2.LUT(src_r, red_lookup_table)
+ 
+    # Put the image back together
+    image_after_matching = cv2.merge([
+        blue_after_transform, green_after_transform, red_after_transform])
+    image_after_matching = cv2.convertScaleAbs(image_after_matching)
+ 
+    return image_after_matching
